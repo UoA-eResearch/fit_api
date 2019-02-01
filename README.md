b7bc26a9fe3bd80c72f0c821fbef0d609916de11
Hil Liao <hilliao@gmail.com>
2019-01-31 19:48:31 -0800

    1. implemented v1 get, post endpoints to get activity, steps, heart rate from Google fitness API and insert to Google Cloud BigQuery tables. 2. Refactored configuration file from config.py to app.config for better configuration management and reducing the risk of pushing database credentials. 3. Updated the readme file with Google App Engine deployment instructions and new configuration instruction.

--- README.md
+++ README.md
@@ -2,20 +2,83 @@
 A web interface to gain consent from a user, to obtain their Google Fit data from the API, such that it can be resurfaced more easily via API  
 
 ## Installation
-
-Copy config.py.example to config.py, replacing with your database credentials  
+1. Follow the instructions to create an OAuth 2.0 Client ID and secret: [https://developers.google.com/fit/rest/v1/get-started]
+2. set APP_CONFIG environment variables to a configuration filename in the project directory. It's app.config by default.
+ Set each values in the configuration file without quotes or double quotes.
+3. Set CLIENT_SECRET environment variables to a path/filename downloaded from the prior step of Client ID for Web application.
+ You can download the file from [https://console.developers.google.com/apis/credentials]. Verify the redirect URIs are correct in the file.
 
 ### Python2
 `sudo apt-get install python-mysqldb` (for debian based distros), or `sudo yum install MySQL-python` (for rpm-based)  
-`sudo pip install -r requirements.txt`  
+`sudo pip install -r requirements.txt`
 
 ### Python3
 `sudo apt-get install python3-mysqldb` or `sudo yum install MySQL-python3`  
 `sudo pip3 install -r requirements.txt`  
 
-Download your OAuth 2.0 client ID JSON file from https://console.developers.google.com/apis/credentials. Also configure this with the redirect URIs you will be using  
+### Create tables in Database and BigQuery
 
-`sudo mysql < structure.sql` to create the database and tables
+`sudo mysql < structure.sql` to create the database and tables. Create the tables in Google Cloud BigQuery ->
+**activities table**
+
+|Field name|Type|Mode|
+|----------|----|----|
+|username|STRING|REQUIRED|
+|recordedLocalDate|DATE|REQUIRED|
+|activity_type|INTEGER|REQUIRED|
+|seconds|INTEGER|NULLABLE|
+|segments|INTEGER|NULLABLE|
+
+**activity_segments**
+
+|Field name|Type|Mode|
+|----------|----|----|
+|username|STRING|REQUIRED|
+|recordedLocalDate|DATE|REQUIRED|
+|activity_type|INTEGER|REQUIRED|
+|startTimeNanos|STRING|NULLABLE|
+|endTimeNanos|STRING|NULLABLE|
+|originDataSourceId|STRING|NULLABLE|
+
+**activity_types**
+
+|Field name|Type|Mode|
+|----------|----|----|
+|id|INTEGER|REQUIRED|
+|name|STRING|NULLABLE|
+
+**heartrate**
+
+|Field name|Type|Mode|
+|----------|----|----|
+|username|STRING|REQUIRED|
+|recordedTimeNanos|INTEGER|REQUIRED|
+|recordedLocalDate|DATE|NULLABLE|
+|bpm|INTEGER|REQUIRED|
+
+insert acticity types into activity_types table by running the section of structure.sql:
+```INSERT INTO `activity_types` (name, id) VALUES```.
+Modify the syntax to fit [https://cloud.google.com/bigquery/docs/reference/standard-sql/query-syntax]
+
+## Deploy to Google App Engine Flexible
+1. Initialize and ready Google Cloud Platform App Engine, BigQuery from Google Cloud Console. Create a service account for local debugging that has the roles of
+ BigQuery Data Editor,
+BigQuery Job User,
+Cloud Datastore User.
+2. Add the same roles to the service account created by App Engine: `service-0000000000@gae-api-prod.google.com.iam.gserviceaccount.com`, 'App Engine Flexible Environment Service Agent'.
+3. app.yaml: set service to the default service or desired service in App Engine
+4. app.yaml: set cloud_sql_instances: Your Instance connection name
+5. `gcloud app deploy` [https://cloud.google.com/appengine/docs/flexible/python/testing-and-deploying-your-app]
+
+### Environment variables:
+1. client secret filename in the project directory as CLIENT_SECRET: client_secret_file.json
+2. set the application default credential to the service account that has BigQuery Data Editor, BigQuery Job User, Cloud Datastore User role as GOOGLE_APPLICATION_CREDENTIALS: /path/to/GCP_service_account.json. Avoid putting the path in the same directory to reduce the chances of pushing it to source control.
+3. app.yaml: change CLIENT_SECRET environment variables to client secret filename in the project directory.
+4. app.yaml: change APP_CONFIG environment variables to a configuration filename in the project directory.
+
+### Python packages
+Add contents in requirements-gae.txt to requirements.txt for App Engine to download the dependent packages.
+
 
 ## Running
 
--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from collections import OrderedDict
 import bottle_mysql
 import httplib2
 from bottle import *
+from google.cloud import datastore
 from googleapiclient.discovery import build
 from oauth2client import client
-
-import config
-from update_google_fit import get_and_load_heart_rate
+from datetime import datetime
+import backend
 from update_google_fit import get_and_store_fit_data
 
 bad_activities = "(0,3,5,109,110,111,112,117,118)"
@@ -18,15 +18,12 @@ bad_activities = "(0,3,5,109,110,111,112,117,118)"
 app = Bottle()
 application = app
 # dbhost is optional, default is localhost
-plugin = bottle_mysql.Plugin(dbhost=config.dbhost, dbport=config.dbport, dbuser=config.dbuser, dbpass=config.dbpass,
-                             dbname=config.dbname)
+plugin = bottle_mysql.Plugin(dbhost=backend.config.get('database_config', 'dbhost'),
+                             dbport=int(backend.config.get('database_config', 'dbport')),
+                             dbuser=backend.config.get('database_config', 'dbuser'),
+                             dbpass=backend.config.get('database_config', 'dbpass'),
+                             dbname=backend.config.get('database_config', 'dbname'))
 app.install(plugin)
-
-# init environment variables
-if 'CLIENT_SECRET' in os.environ:
-    client_secret_file = os.environ['CLIENT_SECRET']
-else:
-    client_secret_file = 'client_secret.json'
 
 # Google Cloud Stackdriver Debugger
 try:
@@ -40,7 +37,7 @@ except ImportError as e:
 
 def require_key():
     key = request.query.get('key', '')
-    if key != config.API_KEY:
+    if key != backend.API_key:
         abort(httplib.UNAUTHORIZED, "invalid API key")
 
 
@@ -49,8 +46,7 @@ def health_check(db):
     if db:
         return "database connection alive: " + str(db.connection)
     else:
-        response.status = httplib.INTERNAL_SERVER_ERROR
-        return "database connection is in a bad state"
+        abort(httplib.INTERNAL_SERVER_ERROR, "database connection is in a bad state")
 
 
 @app.get('/')
@@ -60,7 +56,7 @@ def default_get(db):
     redirect_uri = "{}://{}{}".format(p.scheme, p.netloc, p.path)
 
     flow = client.flow_from_clientsecrets(
-        client_secret_file,
+        backend.client_secret_file,
         scope=["profile", "email", 'https://www.googleapis.com/auth/fitness.activity.read',
                'https://www.googleapis.com/auth/fitness.body.read'],
         redirect_uri=redirect_uri)
@@ -84,6 +80,108 @@ def default_get(db):
         steps, activity = get_and_store_fit_data(http_auth, db, name)
         response.content_type = 'application/json'
         return json.dumps(dict(steps), sort_keys=True, indent=4)
+
+
+@app.get('/oauth2callback')
+def oauth2callback(db):
+    urlparts = request.urlparts
+    redirect_uri = "{}://{}{}".format(urlparts.scheme, urlparts.netloc, urlparts.path)
+
+    flow = client.flow_from_clientsecrets(
+        backend.client_secret_file,
+        scope=["profile", "email", 'https://www.googleapis.com/auth/fitness.activity.read',
+               'https://www.googleapis.com/auth/fitness.body.read'],
+        redirect_uri=redirect_uri)
+    flow.params['access_type'] = 'offline'
+    flow.params['prompt'] = 'consent'
+    creds = flow.step2_exchange(code=request.query.code)
+    http_auth = creds.authorize(httplib2.Http())
+    user_info_service = build('oauth2', 'v2', http=http_auth)
+    get_user_task = user_info_service.userinfo().get()
+    ds = datastore.Client()
+    u = get_user_task.execute()
+
+    # insert to cloud SQL
+    db.execute(
+        "REPLACE INTO google_fit SET username=%s, google_id=%s, full_name=%s, gender=%s, image_url=%s, email=%s, refresh_token=%s",
+        (u['email'], u['id'], u['name'], u.get('gender'), u['picture'], u['email'], creds.refresh_token))
+
+    # insert to Cloud Datastore
+    entity = datastore.Entity(key=ds.key('credentials', u['email']))
+    now = datetime.utcnow()
+    entity.update({
+        'refresh_token': creds.refresh_token,
+        'google_id': u['id'],
+        'gender': u.get('gender'),
+        'picture': u['picture'],
+        'last_updated': now
+    })
+    ds.put(entity)
+    response.content_type = 'application/json'
+
+    # required to serialize entity
+    entity['last_updated'] = now.strftime('%Y-%m-%d %H:%M:%S %Z')
+    return json.dumps(entity.items())
+
+
+@app.post('/v1/users/<username>/steps')
+def insert_steps(username):
+    steps = get_steps(username)
+    insert_result = {
+        'inserted_count': backend.insert_steps(username, steps),
+        'steps': steps
+    }
+
+    response.content_type = 'application/json'
+    return insert_result
+
+
+@app.get('/v1/users/<username>/steps')
+def get_steps(username):
+    assert_headers_apikey()
+    http_auth = get_http_auth_google_apis(username)
+    end_time_millis, start_date = extract_header_dates()
+
+    # end_time_millis in headers data is optional
+    if end_time_millis is None:
+        steps = backend.get_daily_steps(http_auth, start_date['year'], start_date['month'], start_date['day'])
+    else:
+        steps = backend.get_daily_steps(http_auth,
+                                        start_date['year'], start_date['month'], start_date['day'], end_time_millis)
+    response.content_type = 'application/json'
+    return steps
+
+
+@app.get('/v1')
+def main():
+    return static_file("post.html", ".")
+
+
+@app.post('/v1/auth')
+def google_auth():
+    parts = request.urlparts
+    redirect_uri = "{}://{}/oauth2callback".format(parts.scheme, parts.netloc)
+
+    flow = client.flow_from_clientsecrets(
+        backend.client_secret_file,
+        scope=["profile", "email", 'https://www.googleapis.com/auth/fitness.activity.read',
+               'https://www.googleapis.com/auth/fitness.body.read'],
+        redirect_uri=redirect_uri)
+    flow.params['access_type'] = 'offline'
+    flow.params['prompt'] = 'consent'
+    assert_forms_apikey()
+    auth_uri = flow.step1_get_authorize_url()
+    redirect(auth_uri)
+
+
+def assert_headers_apikey():
+    if 'apikey' not in request.headers or request.headers['apikey'] != backend.API_key:
+        abort(httplib.UNAUTHORIZED, "invalid API key in {}".format("request.headers['apikey']"))
+
+
+def assert_forms_apikey():
+    if 'apikey' not in request.forms or request.forms['apikey'] != backend.API_key:
+        abort(httplib.UNAUTHORIZED, "invalid API key in {}".format("request.forms['apikey']"))
 
 
 @app.get('/steps_for_user/<name>')
@@ -110,6 +208,85 @@ def activity_for_user(name, db):
     return json.dumps(result, sort_keys=True, indent=4)
 
 
+@app.get('/users/<name>/activities')
+def user_activities(name, db):
+    require_key()
+    activities = query_activities(db, name)
+    response.content_type = 'application/json'
+    return json.dumps(activities, sort_keys=True, indent=4)
+
+
+def query_activities(db, name):
+    db.execute(
+        "SELECT a.day, ROUND(a.length_ms / 1000 / 60) AS minutes, t.name as activity_type FROM activity a INNER JOIN activity_types t ON a.activity_type=t.id WHERE a.username=%s",
+        (name,))
+    result = db.fetchall()
+    activities = {}
+    for r in result:
+        if r['day'] in activities:
+            if 'daily_activities' not in activities[r['day']]:
+                activities[r['day']]['daily_activities'] = []
+
+            activities[r['day']]['daily_activities'].append(
+                {"minutes": int(r['minutes']), "activity_type": r['activity_type']})
+
+        else:
+            activities[r['day']] = {}
+            activities[r['day']]['daily_activities'] = []
+            activities[r['day']]['daily_activities'].append(
+                {"minutes": int(r['minutes']), "activity_type": r['activity_type']})
+    return activities
+
+
+@app.post('/v1/users/<username>/activities')
+def insert_user_activities(username):
+    activities = get_user_activities(username)
+
+    insert_result = {
+        'inserted_count': backend.insert_activities(username, activities),
+        'activities': activities
+    }
+
+    response.content_type = 'application/json'
+    return insert_result
+
+
+@app.get('/v1/users/<username>/activities')
+def get_user_activities(username):
+    assert_headers_apikey()
+    http_auth = get_http_auth_google_apis(username)
+    end_time_millis, start_date = extract_header_dates()
+
+    # end_time_millis in headers data is optional
+    if end_time_millis is None:
+        activities = backend.get_activities(http_auth, start_date['year'], start_date['month'], start_date['day'])
+    else:
+        activities = backend.get_activities(http_auth,
+                                            start_date['year'], start_date['month'], start_date['day'], end_time_millis)
+
+    response.content_type = 'application/json'
+    return activities
+
+
+def extract_header_dates():
+    # parse headers data in request body
+    start_date = {'year': request.headers.get('start_year', None), 'month': request.headers.get('start_month', None),
+                  'day': request.headers.get('start_day', None)}
+    end_time_millis = request.headers.get('end_time_millis', None)
+    if end_time_millis is not None:
+        end_time_millis = int(end_time_millis)
+    if start_date['year'] is None or start_date['month'] is None or start_date['day'] is None:
+        abort(httplib.BAD_REQUEST, "headers did not contain start_year, start_month, start_day")
+    else:
+        start_date['year'] = int(start_date['year'])
+        start_date['month'] = int(start_date['month'])
+        start_date['day'] = int(start_date['day'])
+
+    return end_time_millis, start_date
+
+
+# bug: only 1 activity per day returned. correct result has multiple activities per day.
+# Fix: refer to @app.get('/users/<name>/activities')
 @app.get('/activity_for_user_details/<name>')
 def activity_for_user_details(name, db):
     require_key()
@@ -213,32 +390,38 @@ def set_goal(name, goal, db):
     return "Goal set"
 
 
-@app.get('/heart/<user>')
-def load_heart_rate(user, db):
-    with open(client_secret_file) as f:
+@app.post('/v1/users/<username>/heart')
+def insert_heart_rate(username):
+    assert_headers_apikey()
+    http_auth = get_http_auth_google_apis(username)
+    end_time_millis, start_date = extract_header_dates()
+
+    # end_time_millis in form data is optional
+    if end_time_millis is None:
+        result = backend.get_and_insert_heart_rate(http_auth, username,
+                                                   start_date['year'], start_date['month'], start_date['day'])
+    else:
+        result = backend.get_and_insert_heart_rate(http_auth, username,
+                                                   start_date['year'], start_date['month'], start_date['day'],
+                                                   end_time_millis)
+    response.content_type = 'application/json'
+    return result
+
+
+def get_http_auth_google_apis(user):
+    with open(backend.client_secret_file) as f:
         client_secret_json = json.load(f)
         client_id = client_secret_json['web']['client_id']
         client_secret = client_secret_json['web']['client_secret']
-
-    n_rows = db.execute("SELECT * FROM google_fit WHERE username = '{}'".format(user))
-    if n_rows == 0:
-        response.status == httplib.NOT_FOUND
-        return "user {} not found in the google_fit table".format(user)
-    rows = db.fetchall()
-    for r in rows:
-        username = r['username']
-        refresh_token = r['refresh_token']
-        creds = client.GoogleCredentials(None, client_id, client_secret, refresh_token, None,
-                                         "https://accounts.google.com/o/oauth2/token", "Python")
-        http_auth = creds.authorize(httplib2.Http())
-        try:
-            result = get_and_load_heart_rate(http_auth, username)
-        except Exception as e:
-            response.status = httplib.INTERNAL_SERVER_ERROR
-            return "Failed to get or insert fitness data for user = {}: {}".format(username, e)
-
-    response.content_type = 'application/json'
-    return result
+    ds = datastore.Client()
+    key = ds.key('credentials', user)
+    user_creds = ds.get(key)
+    assert user_creds.key.id_or_name == user
+    refresh_token = user_creds['refresh_token']
+    creds = client.GoogleCredentials(None, client_id, client_secret, refresh_token, None,
+                                     "https://accounts.google.com/o/oauth2/token", "Python")
+    http_auth = creds.authorize(httplib2.Http())
+    return http_auth
 
 
 port = int(os.environ.get('PORT', 8080))
